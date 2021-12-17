403ac9926f0e6eb205f38479b32ccead0ab9308c
viktornuts <viktornuts@gmail.com>
2021-12-17 17:59:19 +0500

    added change ToWork 4

--- src/test/java/guru/qa/tests/ToWork.java
+++ src/test/java/guru/qa/tests/ToWork.java
@@ -20,11 +20,18 @@ public class ToWork {
 
     @Test
     void fillFromTest(){
+
+        //Логин
+
         open("https://mchd-front-ui-mchd-tst.tst.cld.esphere.local");
         $("#IDToken1").setValue("romashkaa");
         $("#IDToken2").setValue("12345678");
         $("[name='Login.Submit']").click();
+
+        //Главная страница сервиса МЧД - создать
         $("[class='button-wrapper success']").click();
+
+        //Полномочия по доверенности
 
         $("[class='datepicker-calendar-icon']").click();
         $("[class='calendar-title']").click();
@@ -32,8 +39,6 @@ public class ToWork {
         $("[title='2019']").click();
         $("[title='Март']").click();
         $("[title='5 марта 2019']").click();
-
-
 
         $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[2]/dd[1]/div/div[2]/div/span/span")).click();
         $("[class='calendar-title']").click();
@@ -46,51 +51,61 @@ public class ToWork {
         $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[2]/dd[2]/div/div[1]/div")).click();
         $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[2]/dd[2]/div/div[2]/div[2]/div/ul/li[1]/div/div")).click();
 
+        //Сведения о доверителе
 
         $("[name='companyName']").setValue("ООО \"Рога и копыта\"");
         $("input[name='inn']").setValue("4431602938");
         $("input[name='kpp']").setValue("270545600");
-        $("input[name='ogrnip']").setValue("689701451573");
+        $("input[name='ogrnip']").setValue("6083327334755");
         $("input[name='address']").setValue("Москва ул.Пушкина дом 1");
-        $("input[name='chief_lastName']").setValue("Петров");
+        $("input[name='chief_lastName']").setValue("Доверчивый");
         $("input[name='chief_firstName']").setValue("Петр");
+        $("input[name='chief_middleName']").setValue("Петрович");
+        $("input[name='position']").setValue("Директор по экономике");
+        $("input[name='chief_inn']").setValue("476826216095");
+        $("input[name='chief_snils']").setValue("85889150482").pressEnter();
+
+        //Сведенеия о представителе (доверенной стороне)
+
+        $("input[name='confidant_lastName']").setValue("Подписантов");
+        $("input[name='confidant_firstName']").setValue("Олег");
+        $("input[name='confidant_middleName']").setValue("Иванович");
+
+        $("input[name='confidant_inn']").scrollIntoView(true);
+
+        $("input[name='confidant_inn']").setValue("832231002384");
+        $("input[name='confidant_snils']").setValue("201-531-994 22");
+
+        $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[6]/dd[3]/div/div/span/span")).click();
+        $("#calendar-title").click();
+        $("#calendar-title").click();
+        $("#calendar-prev-icon").click();
+        $("#calendar-prev-icon").click();
+        $("[title='1990']").click();
+        $("[title='Июнь']").click();
+        $("[title='22 июня 1990']").click();
+
+        //Документ удостоверяющий личность
+
+        $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[7]/dl/dd[1]/div/div/span")).click();
+        $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[7]/dl/dd[1]/div/div[2]/ul/li[1]")).click();
+        $("input[name='documentNumber']").setValue("7512203313");
+
+        $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[7]/dl/dd[3]/div/div/span/span")).click();
+        $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[6]/dd[3]/div/div[2]/div[1]/span[2]")).click();
+        $(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/dl[6]/dd[3]/div/div[2]/div[1]/span[2]")).click();
+        $("#calendar-prev-icon").click();
+        $("[title='2012']").click();
+        $("[title='Январь']").click();
+        $("[title='22 января 2012']").click();
+
+        $("#button-wrapper.success").click();
 
 
 
-        $("[class='react-datepicker__month-select']").selectOption("June");
-        $("[class='react-datepicker__year-select']").selectOption("1990");
-        $("[class*='react-datepicker__day--021']").click();
 
-
-        $("#userEmail").setValue("viktornuts@gmail.com");
-        $("[for='gender-radio-1']").click();
-        $("#userNumber").setValue("8955245541");
-
-        $("#dateOfBirthInput").click();
-
-        $("[class='react-datepicker__year-select']").selectOption("1990");
-        $("[class*='react-datepicker__day--021']").click();
-
-        $("#subjectsInput").setValue("English").pressEnter();
-        $("#subjectsInput").setValue("Maths").pressEnter();
-
-        $("[for='hobbies-checkbox-1']").click();
-        $("[for='hobbies-checkbox-2']").click();
-
-        File lesson = new File("src/test/java/guru/qa/files/lesson1.png");
-        String path = lesson.getAbsolutePath();
-        $("#uploadPicture").sendKeys(path);
-
-        $("[placeholder='Current Address']").setValue("Nikolaya Shishka 21");
-        $("[placeholder='Current Address']").scrollIntoView(true);
-        $("#react-select-3-input").setValue("Raj").pressEnter();
-        $("#react-select-4-input").setValue("Jaise").pressEnter();
-        $("#submit").click();
 
         //Assert
-        $$x("//*[@class='modal-body']//td[2]").shouldHave(CollectionCondition.exactTexts(
-                "Viktor Slon", "viktornuts@gmail.com", "Male", "8955245541", "21 June,1990",
-                "English, Maths", "Sports, Reading", "lesson1.png", "Nikolaya Shishka 21", "Rajasthan Jaiselmer"));
 
     }
 }
