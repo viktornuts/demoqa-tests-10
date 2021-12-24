a8871a257e94e6d3f1d081ad28c3b670d4e5ddbf
viktornuts <viktornuts@gmail.com>
2021-12-24 11:30:03 +0500

    added lesson3

---
+++ src/test/java/guru/qa/tests/Lesson3.java
@@ -0,0 +1,57 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class Lesson3 {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+
+    }
+
+    @AfterEach
+    void after(){
+        closeWebDriver();
+    }
+
+    /*1. Разница между
+
+- $("h1").$("div"): сперва мы ищем первый "h1", внутри первого найденного элемента "h1" мы ищем первый элемент "div"
+
+- $("h1 div"): мы ищем первый "div" вложенный в элемент "h1"
+*/
+
+
+    @Test
+    void shouldFindCodeJUnit5InSoftAssertions()  {
+
+
+
+        open("https://github.com/");
+        $("input[data-test-selector='nav-search-input']").setValue("Selenide").pressEnter();
+        $("nav[class='menu border d-none d-md-block']").$(byText("Wikis")).shouldBe(visible).click();
+        $("#wiki_search_results").shouldHave(text("SoftAssertions"));
+        $("#wiki_search_results").$("a[title='SoftAssertions']").click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:")).shouldBe(visible);
+
+    }
+}
